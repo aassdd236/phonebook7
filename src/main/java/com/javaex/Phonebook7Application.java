ca4fb1cc2d4915fcf03105f0b554e135e10bb62e
aassdd236 <aassdd236@naver.com>
2024-03-05 16:15:20 +0900

    03/05

---
+++ src/main/java/com/javaex/Phonebook7Application.java
@@ -0,0 +1,13 @@
+package com.javaex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phonebook7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phonebook7Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/javaex/dao/PhonebookDao.java
@@ -0,0 +1,26 @@
+package com.javaex.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.PersonVo;
+
+@Repository
+public class PhonebookDao {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public int personInsert(PersonVo personVo) {
+		System.out.println("PhonebookDao.personInsert()");
+		
+		System.out.println(personVo);
+		
+		int count=sqlSession.insert("phonebook.insert", personVo);
+		System.out.println(count);
+		return count;
+		
+	}
+
+}
